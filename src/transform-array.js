15eb68d466c7f9b81176340de9bcdc76ea2af787
malashonock <malashonock@gmail.com>
2021-10-02 23:11:09 +0300

    feat: implement transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,61 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+transform([1, 2, 3, '--discard-next', 1337, '--double-prev', 4, 5]);
+
+export default function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  let action = null;
+  const transformed = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const el = arr[i];
+
+    switch (el) {
+      case '--double-next':
+        action = 'double';
+        break;
+      case '--discard-next':
+        action = 'discard';
+        break;
+      case '--double-prev':
+        if (transformed.length > 0) {
+          if (arr[i - 2] && arr[i - 2] != '--discard-next') {
+            transformed.push(arr[i - 1]);
+          }
+        }
+        break;       
+      case '--discard-prev':
+        if (transformed.length > 0) {
+          if (arr[i - 2] && arr[i - 2] != '--discard-next') {
+            transformed.pop();
+          }
+        }
+        break;
+      default:
+        if (!action) {
+          transformed.push(el);          
+        } else {
+          switch (action) {
+            case 'double':
+              transformed.push(el)
+              transformed.push(el);
+              action = null;
+              break;
+            case 'discard':
+              // skip element
+              action = null;
+            default:
+              break;
+          }
+        }
+        break;
+    }
+  }
+
+  return transformed;
 }
