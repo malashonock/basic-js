87571f8eb2b56308dbfd32630c7e02704c2bf41b
malashonock <malashonock@gmail.com>
2021-10-03 14:06:51 +0300

    feat: implement simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,25 +4,41 @@ import { NotImplementedError } from '../extensions/index.js';
  * Implement chainMaker object according to task description
  * 
  */
+
+// const chainMaker = {
 export default {
+  links: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.links.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.links.push(value);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (isNaN(position) 
+      || position < 1
+      || position > this.links.length) {
+      this.links = [];
+      throw new Error('You can\'t remove incorrect link!');
+    }
+
+    this.links.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.links.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const toString = this.links
+      .map(linkValue => `( ${linkValue} )`)
+      .join('~~');
+
+    this.links = [];
+    return toString;    
   }
 };
+
+// let chain = chainMaker;
+// chainMaker.addLink('GHI').addLink(null).reverseChain().addLink(333).reverseChain().reverseChain().addLink(0).reverseChain().reverseChain().addLink('GHI').finishChain();
