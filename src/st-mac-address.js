51a41b1c6ba24169b09edbe549502d145f05734e
Yan Malashonak <malashonock@gmail.com>
2021-10-03 18:42:10 +0300

    feat: implement mac address

--- src/st-mac-address.js
+++ src/st-mac-address.js
@@ -14,7 +14,7 @@ import { NotImplementedError } from '../extensions/index.js';
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-export default function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function isMAC48Address(n) {
+  let regexp = new RegExp(Array(6).fill('[\\dA-F]{2}').join('-'))
+  return n.search(regexp) >= 0;
 }
